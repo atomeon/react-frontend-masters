679ec0082aba19e1cb7f4676a8bdc38b57943bb1
atomeon <atomeon@gmail.com>
2019-04-13 23:59:40 +0600

    feat: getting API from petfinder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import { Pet } from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
   handleTitleClick() {
     console.log("clicked on a title");
+  }
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+    promise.then(console.log, console.error);
   }
   render() {
     // return React.createElement('div', {}, [
@@ -28,17 +38,17 @@ class App extends React.Component {
 
     // 		name: 'Buran',
     // 		animal: 'dog',
-    // 		breed: 'prosto pes'
+    // 		breed: 'prosto'
     // 	}),
     // ])
 
     return (
-      <>
+      <div>
         <h1>Adopt me!</h1>
         <Pet name="Luna" animal="dog" breed="Ovcharka" />
         <Pet name="Ralph" animal="dog" breed="Ovcharka" />
         <Pet name="Buran" animal="dog" breed="Yardy" />
-      </>
+      </div>
     );
   }
 }
