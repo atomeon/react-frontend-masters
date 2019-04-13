c03b00ad95170107df4b9afcfcf3c114ed717dd5
atomeon <atomeon@gmail.com>
2019-04-14 00:32:03 +0600

    feat: getting data from API via promise

--- src/App.js
+++ src/App.js
@@ -9,12 +9,33 @@ const petfinder = pf({
 });
 
 class App extends React.Component {
-  handleTitleClick() {
-    console.log("clicked on a title");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
   }
   componentDidMount() {
-    const promise = petfinder.breed.list({ animal: "dog" });
-    promise.then(console.log, console.error);
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
   }
   render() {
     // return React.createElement('div', {}, [
