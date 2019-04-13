284a6f00b0a20cef919db3420fb1406381f13103
atomeon <atomeon@gmail.com>
2019-04-13 22:18:56 +0600

    feat: prettier

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import React from 'react';
+import React from "react";
 import { render } from "react-dom";
-import { Pet } from './Pet';
+import { Pet } from "./Pet";
 
 class App extends React.Component {
-	handleTitleClick() {
-		console.log('clicked on a title');
-	}
-	render() {
-		// return React.createElement('div', {}, [
-		// 	React.createElement(
-		// 		'h1',
-		// 		{ onClick: this.handleTitleClick },
-		// 		'Adopt me!'),
-		// 	React.createElement(Pet, {
+  handleTitleClick() {
+    console.log("clicked on a title");
+  }
+  render() {
+    // return React.createElement('div', {}, [
+    // 	React.createElement(
+    // 		'h1',
+    // 		{ onClick: this.handleTitleClick },
+    // 		'Adopt me!'),
+    // 	React.createElement(Pet, {
 
-		// 		name: 'Lunak',
-		// 		animal: 'dog',
-		// 		breed: 'hovenese'
-		// 	}),
-		// 	React.createElement(Pet, {
+    // 		name: 'Lunak',
+    // 		animal: 'dog',
+    // 		breed: 'hovenese'
+    // 	}),
+    // 	React.createElement(Pet, {
 
-		// 		name: 'Tuzik',
-		// 		animal: 'dog',
-		// 		breed: 'dvornyaga'
-		// 	}),
-		// 	React.createElement(Pet, {
+    // 		name: 'Tuzik',
+    // 		animal: 'dog',
+    // 		breed: 'dvornyaga'
+    // 	}),
+    // 	React.createElement(Pet, {
 
-		// 		name: 'Buran',
-		// 		animal: 'dog',
-		// 		breed: 'prosto pes'
-		// 	}),
-		// ])
+    // 		name: 'Buran',
+    // 		animal: 'dog',
+    // 		breed: 'prosto pes'
+    // 	}),
+    // ])
 
-		return (
-			<>
-				<h1 onClick={this.handleTitleClick} >Adopt me!</h1>
-				<Pet name="Luna" animal="dog" breed="Ovcharka" />
-				<Pet name="Ralph" animal="dog" breed="Ovcharka" />
-				<Pet name="Buran" animal="dog" breed="Yardy" />
-			</>
-		)
-	}
-};
+    return (
+      <>
+        <h1 onClick={this.handleTitleClick}>Adopt me!</h1>
+        <Pet name="Luna" animal="dog" breed="Ovcharka" />
+        <Pet name="Ralph" animal="dog" breed="Ovcharka" />
+        <Pet name="Buran" animal="dog" breed="Yardy" />
+      </>
+    );
+  }
+}
 
-
-render(
-	React.createElement(App),
-	document.getElementById('root')
-);
+render(React.createElement(App), document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-
-export const Pet = (props) => {
-	return (
-		<div>
-			<h1>{props.name}</h1>
-			<h2>{props.animal}</h2>
-			<h2>{props.breed}</h2>
-		</div>
-	);
+export const Pet = props => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
-
