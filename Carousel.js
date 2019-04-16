5372a610c1be6236a4eb6236851c5737ef61e4fc
Timur <atomeon@gmail.com>
2019-04-16 16:30:41 +0600

    feat: carousel interactive

--- Carousel.js
+++ Carousel.js
@@ -5,12 +5,23 @@ class Carousel extends React.Component {
 		photos : [],
 		active : 0
 	}
+
+	constructor(props) {
+		super(props); 
+
+		this.handleIndexClick = this.handleIndexClick.bind(this);
+	}
 	static getDerivedStateFromProps({ media }) { 
 		let photos = [];
 		if (media && media.photos && media.photos.photo) {
 			photos = media.photos.photo.filter(photo => photo['@size'] === "pn")
 		}
 		return { photos }
+	}
+	handleIndexClick = (event) => {
+		this.setState({
+			active: +event.target.dataset.index
+		})
 	}
 	render (){
 		const { photos, active } = this.state;
@@ -21,10 +32,12 @@ class Carousel extends React.Component {
 				<div className="carousel-smaller">
 					{photos.map((photo, index) => (
 						<img 
+							onClick={this.handleIndexClick}
 							key={photo.value}
 							src={photo.value}
 							className={ index === active ? 'active' : '' }
 							alt="animal thumbnail" 
+							data-index={index}
 						/>
 					))}
 				</div>
