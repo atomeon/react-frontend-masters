950340471b827a0a593e6701a356c1c77b61b23e
Timur <atomeon@gmail.com>
2019-04-16 15:46:53 +0600

    feat: carousel of pictures in profile

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pf from 'petfinder-client';
 import { navigate } from "@reach/router";
+import Carousel from '../Carousel';
 
 const petfinder = pf({
 	key: process.env.API_KEY,
@@ -41,9 +42,10 @@ class Details extends React.Component {
 		if(this.state.loading) {
 			return <h1>Loading...</h1>
 		}
-		const { name, animal, breed, location, description } = this.state;
+		const { name, animal, breed, location, description, media } = this.state;
 		return (
 			<div className="details">
+			<Carousel media={media}/>
 				<div>
 					<h1>{name}</h1>
 					<h2>{animal} - {breed} - {location}</h2>
